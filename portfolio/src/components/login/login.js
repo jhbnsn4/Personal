50951fe538faa9dd8ca66672d72fdc5d16146234
john benson <jdbensonwiu@gmail.com>
2022-08-20 20:06:45 -0700

    input styling

--- portfolio/src/components/login/login.js
+++ portfolio/src/components/login/login.js
@@ -1,11 +1,14 @@
 import React from "react";
+import './login.css'
 
 export default function Login() {
   return (
-    <div>
+    <>
+    <div className="login-wrapper">
       <h1>Login</h1>
-      <input></input>
-      <input></input>
+      <input className="login-input"></input>
+      <input className="login-input"></input>
     </div>
+    </>
   );
 }
